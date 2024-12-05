ee7e6ba523bcace4441764f6af0d9ce099505639
Tonyecole89 <tony.yam@edu.ecole-89.com>
2024-12-05 18:46:08 +0100

    update

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,8 +11,21 @@ class TestController extends AbstractController
     #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
+        
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
+        ]);
+        
+        /*return $this->json(['username' => 'jane.doe']);*/
+    }
+
+    #[Route('/test/{id}', name: 'test_id', methods: ["GET"], requirements: ['id' => '\d+'])]
+    public function test_id($id): Response
+    {
+        //dd($id);
+        return $this->render('test/test.html.twig', [
+            'controller_name' => 'TestController',
+            'id' => $id,
         ]);
     }
 }
